15f7c2adf9c224ecd7fdce9aa30273cee9f4b529
sctanf <36978460+sctanf@users.noreply.github.com>
2025-05-10 23:57:07 -0500

    lsm add int config to init

--- src/sensor/imu/LSM6DSO.c
+++ src/sensor/imu/LSM6DSO.c
@@ -24,7 +24,7 @@ int lsm6dso_init(float clock_rate, float accel_time, float gyro_time, float *acc
 {
 	// setup interface for SPI
 	sensor_interface_spi_configure(SENSOR_INTERFACE_DEV_IMU, MHZ(10), 0);
-	int err = ssi_reg_write_byte(SENSOR_INTERFACE_DEV_IMU, LSM6DSO_CTRL3, 0x44); // freeze register until done reading, increment register address during multi-byte access (BDU, IF_INC)
+	int err = ssi_reg_write_byte(SENSOR_INTERFACE_DEV_IMU, LSM6DSO_CTRL3, 0x74); // freeze register until done reading, increment register address during multi-byte access (BDU, IF_INC), INT H_LACTIVE active low, PP_OD open-drain
 	if (err)
 		LOG_ERR("Communication error");
 	last_accel_odr = 0xff; // reset last odr
@@ -320,7 +320,7 @@ uint8_t lsm6dso_setup_WOM(void)
 
 	err |= ssi_reg_write_byte(SENSOR_INTERFACE_DEV_IMU, LSM6DSO_TAP_CFG2, 0x80); // enable interrupts
 	err |= ssi_reg_write_byte(SENSOR_INTERFACE_DEV_IMU, LSM6DSO_MD1_CFG, 0x20); // route wake-up to INT1
-	err |= ssi_reg_write_byte(SENSOR_INTERFACE_DEV_IMU, LSM6DSO_CTRL3, 0x30); // INT1 H_LACTIVE active low, PP_OD open-drain
+	err |= ssi_reg_write_byte(SENSOR_INTERFACE_DEV_IMU, LSM6DSO_CTRL3, 0x30); // INT H_LACTIVE active low, PP_OD open-drain
 	if (err)
 		LOG_ERR("Communication error");
 	return NRF_GPIO_PIN_PULLUP << 4 | NRF_GPIO_PIN_SENSE_LOW; // active low
