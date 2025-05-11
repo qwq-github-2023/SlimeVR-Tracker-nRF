ed87e8f5304a265446c4c6d01385723a92f87b7c
sctanf <36978460+sctanf@users.noreply.github.com>
2025-05-10 22:44:06 -0500

    lsm set int config to open drain active low

--- src/sensor/imu/LSM6DSV.c
+++ src/sensor/imu/LSM6DSV.c
@@ -424,9 +424,10 @@ uint8_t lsm_setup_WOM(void)
 
 	err |= ssi_reg_write_byte(SENSOR_INTERFACE_DEV_IMU, LSM6DSV_FUNCTIONS_ENABLE, 0x80); // enable interrupts
 	err |= ssi_reg_write_byte(SENSOR_INTERFACE_DEV_IMU, LSM6DSV_MD1_CFG, 0x20); // route wake-up to INT1
+	err |= ssi_reg_write_byte(SENSOR_INTERFACE_DEV_IMU, LSM6DSV_CTRL3, 0x30); // INT1 H_LACTIVE active low, PP_OD open-drain
 	if (err)
 		LOG_ERR("Communication error");
-	return NRF_GPIO_PIN_NOPULL << 4 | NRF_GPIO_PIN_SENSE_HIGH; // active high
+	return NRF_GPIO_PIN_PULLUP << 4 | NRF_GPIO_PIN_SENSE_LOW; // active low
 }
 
 int lsm_ext_setup(uint8_t addr, uint8_t reg)
