97c6c471e1db309362f9b9236abd637f95837525
sctanf <36978460+sctanf@users.noreply.github.com>
2025-05-17 17:47:30 -0500

    held button enters pairing

--- src/main.c
+++ src/main.c
@@ -61,7 +61,35 @@ int main(void)
 	uint8_t reboot_counter = reboot_counter_read();
 	bool booting_from_shutdown = !reboot_counter && (reset_pin_reset || button_read()); // 0 means from user shutdown or failed ram validation
 
-	if (booting_from_shutdown)
+	/* if button is not held after booting from shutdown, power off again
+	 * if button press is normal, continue boot
+	 * if button is held for 5 seconds, reset pairing and continue boot
+	 */
+
+	if (button_read())
+	{
+		while (button_read())
+		{
+			if (k_uptime_get() > 1000)
+				set_led(SYS_LED_PATTERN_LONG, SYS_LED_PRIORITY_HIGHEST);
+			if (k_uptime_get() > 5000)
+			{
+				LOG_INF("Pairing requested");
+				esb_reset_pair();
+				break;
+			}
+			k_msleep(1);
+		}
+#if USER_SHUTDOWN_ENABLED
+		if (k_uptime_get() < 50 && booting_from_shutdown) // debounce
+			sys_request_system_off();
+#endif
+		if (k_uptime_get() <= 5000)
+			set_led(SYS_LED_PATTERN_ONESHOT_POWERON, SYS_LED_PRIORITY_HIGHEST);
+		else
+			set_led(SYS_LED_PATTERN_OFF, SYS_LED_PRIORITY_HIGHEST);
+	}
+	else if (booting_from_shutdown)
 		set_led(SYS_LED_PATTERN_ONESHOT_POWERON, SYS_LED_PRIORITY_BOOT);
 
 	bool docked = dock_read();
@@ -73,7 +101,7 @@ int main(void)
 	else if (reboot_counter > 200)
 		reboot_counter = 200; // How did you get here
 	reset_mode = reboot_counter - 100;
-	if ((reset_pin_reset || button_read()) && !docked) // Count pin resets while not docked
+	if (reset_pin_reset && !docked) // Count pin resets while not docked
 	{
 		reboot_counter++;
 		reboot_counter_write(reboot_counter);
@@ -84,7 +112,7 @@ int main(void)
 		k_msleep(1000); // Wait before clearing counter and continuing
 	}
 	reboot_counter_write(100);
-	if (!reset_pin_reset && !button_read() && reset_mode == 0) // Only need to check once, if the button is pressed again an interrupt is triggered from before
+	if (!reset_pin_reset && reset_mode == 0) // Only need to check once, if the button is pressed again an interrupt is triggered from before
 		reset_mode = -1; // Cancel reset_mode (shutdown)
 
 #if USER_SHUTDOWN_ENABLED
