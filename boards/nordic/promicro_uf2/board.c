9e5365dc09eabbc193c232deee5cdfc102d4ddbe
ShineBrightMeow <142170650+ShineBrightMeow@users.noreply.github.com>
2025-08-07 20:00:51 -0700

    fix: Moved vcc-gpios to specific board.c

--- boards/nordic/promicro_uf2/board.c
+++ boards/nordic/promicro_uf2/board.c
@@ -6,15 +6,23 @@
 
 #include <zephyr/init.h>
 #include <hal/nrf_gpio.h>
+#include <zephyr/devicetree.h>
+
+#define ZEPHYR_USER_NODE DT_PATH(zephyr_user)
+
+#define VCC_GPIO_PIN DT_GPIO_PIN(ZEPHYR_USER_NODE, vcc_gpios)
+#define VCC_GPIO_PORT DT_GPIO_LABEL(ZEPHYR_USER_NODE, vcc_gpios)
+
+#define VCC_GPIO_PORT_NUM DT_PROP(DT_GPIO_CTLR(ZEPHYR_USER_NODE, vcc_gpios), port)
 
 static int board_promicro_init(void)
 {
-	/* using p0.31 for sensor power
+	/* using vcc-gpios for sensor power defined in promicro_uf2.dts
 	 */
-	nrf_gpio_cfg(NRF_GPIO_PIN_MAP(0, 31), NRF_GPIO_PIN_DIR_OUTPUT, NRF_GPIO_PIN_INPUT_DISCONNECT, NRF_GPIO_PIN_NOPULL, NRF_GPIO_PIN_D0H1, NRF_GPIO_PIN_NOSENSE);
-	nrf_gpio_pin_set(NRF_GPIO_PIN_MAP(0, 31));
+	nrf_gpio_cfg(NRF_GPIO_PIN_MAP(VCC_GPIO_PORT_NUM, VCC_GPIO_PIN), NRF_GPIO_PIN_DIR_OUTPUT, NRF_GPIO_PIN_INPUT_DISCONNECT, NRF_GPIO_PIN_NOPULL, NRF_GPIO_PIN_D0H1, NRF_GPIO_PIN_NOSENSE);
+	nrf_gpio_pin_set(NRF_GPIO_PIN_MAP(VCC_GPIO_PORT_NUM, VCC_GPIO_PIN));
 	return 0;
 }
 
 SYS_INIT(board_promicro_init, PRE_KERNEL_1,
-	 CONFIG_KERNEL_INIT_PRIORITY_DEFAULT);
+	CONFIG_KERNEL_INIT_PRIORITY_DEFAULT);
