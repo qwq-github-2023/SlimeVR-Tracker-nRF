6095eccc57d93312e846901b5652c710c5720dcd
sctanf <36978460+sctanf@users.noreply.github.com>
2025-10-22 09:32:44 -0500

    scan_spi debug err

--- src/sensor/scan_spi.c
+++ src/sensor/scan_spi.c
@@ -58,6 +58,7 @@ int sensor_scan_spi(struct spi_dt_spec *bus, uint8_t *spi_dev_reg, int dev_addr_
 				LOG_DBG("Scanning register: 0x%02X", reg);
 				// TODO: BMM150 workaround?
 				int err = spi_transceive_dt(bus, &tx, &rx);
+				LOG_DBG("err: %d", err);
 				id = buf[1] ? buf[1] : buf[2]; // ID may be in first byte, or skip one byte (such as BMI270)
 				LOG_DBG("Read value: 0x%02X, 0x%02X, 0x%02X (0x%02X)", buf[0], buf[1], buf[2], id);
 				if (err)
