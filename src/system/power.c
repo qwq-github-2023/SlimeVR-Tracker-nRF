e0f4f4ad6361b2071522eb7b99f1c7d145702fb2
sctanf <36978460+sctanf@users.noreply.github.com>
2025-10-24 05:08:25 -0500

    reorganize power battery calculation

--- src/system/power.c
+++ src/system/power.c
@@ -369,6 +369,69 @@ static void disable_DFU_thread(void)
 #endif
 }
 
+static void update_battery(int16_t battery_pptt)
+{
+	// Plugged state will cause a sudden change in SOC >10%, so reset the sample array
+	if (average_pptt >= 0 && NRFX_ABS(battery_pptt - average_pptt) > 1000)
+	{
+		LOG_INF("Change to battery SOC: %5.2f%% -> %5.2f%%", (double)average_pptt / 100.0, (double)battery_pptt / 100.0);
+		memset(last_pptt, -1, sizeof(last_pptt)); // reset array
+		samples = 1;
+	}
+
+	// Initalize sorted array
+	int16_t sorted_pptt[BATTERY_SAMPLES];
+	memcpy(sorted_pptt, last_pptt, sizeof(last_pptt));
+	sorted_pptt[BATTERY_SAMPLES - 1] = battery_pptt;
+
+	// Now add the last reading to the sample array
+	last_pptt[last_pptt_index] = battery_pptt;
+	last_pptt_index++;
+	last_pptt_index %= BATTERY_SAMPLES - 1;
+
+	// Sort sample array
+	for (int i = 1; i < BATTERY_SAMPLES; i++)
+	{
+		int16_t key = sorted_pptt[i];
+		int8_t j = i - 1;
+		while (j >= 0 && sorted_pptt[j] > key)
+		{
+			sorted_pptt[j + 1] = sorted_pptt[j];
+			j = j - 1;
+		}
+		sorted_pptt[j + 1] = key;
+	}
+
+	// Average across median 75% of samples
+	average_pptt = 0;
+	uint8_t valid_samples = 0;
+	for (uint8_t i = BATTERY_SAMPLES - (samples - samples / 8); i < (BATTERY_SAMPLES - samples / 8); i++)
+	{
+		if (sorted_pptt[i] != -1)
+		{
+			average_pptt += sorted_pptt[i];
+			valid_samples++;
+		}
+	}
+	if (valid_samples > 0)
+		average_pptt /= valid_samples;
+	else
+		average_pptt = battery_pptt;
+
+	// Store the average battery level with hysteresis (Effectively 100-10000 -> 1-100%)
+	if (average_pptt + 100 < hysteresis_pptt) // Lower bound -100pptt
+		hysteresis_pptt = average_pptt + 100;
+	else if (average_pptt > hysteresis_pptt) // Upper bound +0pptt
+		hysteresis_pptt = average_pptt;
+
+	// 0% to battery tracker will reset it, as >1% to 0% is invalid change
+	// Instead, remap 1-100 to 0-100
+	current_battery_pptt = (hysteresis_pptt - 100) * 100 / 99;
+}
+
+// TODO: this thread is handling reading charging state, battery state, dock state, and setting status/led
+// TODO: should be separated to be more clear in its function?
+// TODO: call into other thread for handling the system state
 static void power_thread(void)
 {
 	while (1)
@@ -457,67 +520,13 @@ static void power_thread(void)
 			sys_request_system_off(true);
 		}
 
-		if (battery_available && !battery_low && battery_pptt < 1000)
+		// will update average_pptt, and current_battery_pptt
+		update_battery(battery_pptt);
+
+		if (battery_available && !battery_low && current_battery_pptt < 1000)
 			battery_low = true;
-		else if (!battery_available || (battery_low && battery_pptt > 1500)) // hysteresis
+		else if (!battery_available || (battery_low && current_battery_pptt > 1000)) // hysteresis alrerady provided
 			battery_low = false;
-
-		// Plugged state will cause a sudden change in SOC >10%, so reset the sample array
-		if (average_pptt >= 0 && NRFX_ABS(battery_pptt - average_pptt) > 1000)
-		{
-			LOG_INF("Change to battery SOC: %5.2f%% -> %5.2f%%", (double)average_pptt / 100.0, (double)battery_pptt / 100.0);
-			memset(last_pptt, -1, sizeof(last_pptt)); // reset array
-			samples = 1;
-		}
-
-		// Initalize sorted array
-		int16_t sorted_pptt[BATTERY_SAMPLES];
-		memcpy(sorted_pptt, last_pptt, sizeof(last_pptt));
-		sorted_pptt[BATTERY_SAMPLES - 1] = battery_pptt;
-
-		// Now add the last reading to the sample array
-		last_pptt[last_pptt_index] = battery_pptt;
-		last_pptt_index++;
-		last_pptt_index %= BATTERY_SAMPLES - 1;
-
-		// Sort sample array
-		for (int i = 1; i < BATTERY_SAMPLES; i++)
-		{
-			int16_t key = sorted_pptt[i];
-			int8_t j = i - 1;
-			while (j >= 0 && sorted_pptt[j] > key)
-			{
-				sorted_pptt[j + 1] = sorted_pptt[j];
-				j = j - 1;
-			}
-			sorted_pptt[j + 1] = key;
-		}
-
-		// Average across median 75% of samples
-		average_pptt = 0;
-		uint8_t valid_samples = 0;
-		for (uint8_t i = BATTERY_SAMPLES - (samples - samples / 8); i < (BATTERY_SAMPLES - samples / 8); i++)
-		{
-			if (sorted_pptt[i] != -1)
-			{
-				average_pptt += sorted_pptt[i];
-				valid_samples++;
-			}
-		}
-		if (valid_samples > 0)
-			average_pptt /= valid_samples;
-		else
-			average_pptt = battery_pptt;
-
-		// Store the average battery level with hysteresis (Effectively 100-10000 -> 1-100%)
-		if (average_pptt + 100 < hysteresis_pptt) // Lower bound -100pptt
-			hysteresis_pptt = average_pptt + 100;
-		else if (average_pptt > hysteresis_pptt) // Upper bound +0pptt
-			hysteresis_pptt = average_pptt;
-
-		// 0% to battery tracker will reset it, as >1% to 0% is invalid change
-		// Instead, remap 1-100 to 0-100
-		current_battery_pptt = (hysteresis_pptt - 100) * 100 / 99;
 
 		sys_update_battery_tracker_voltage(battery_mV, device_plugged);
 		if (samples == BATTERY_SAMPLES || device_plugged)
